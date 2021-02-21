774e02631b4dfc05c90d54d5a3593956c9c8c62b
uoneway <uoneway@gmail.com>
2021-02-21 15:34:43 +0900

    Create NER replace and resotore feature  in translator module

--- translator.py
+++ translator.py
@@ -1,4 +1,6 @@
+import re
 import fasttext  # pip install fasttext
+from pororo import Pororo  # pip install pororo
 
 
 BASE_URL = "https://openapi.naver.com/v1/papago/n2mt"
@@ -17,6 +19,15 @@ PORORO_CAN_LANG = {  # https://kakaobrain.github.io/pororo/seq2seq/mt.html
     'zh': ('ko', 'en', 'ja'),
 }
 CAN_LANG = ('ko', 'en')
+
+# print(Pororo.available_models("named_entity_recognition"))
+print("Start to load Pororo")
+ner_models = {
+    'ko': Pororo(task="ner", lang="ko"),
+    'en': Pororo(task="ner", lang="en"),
+    'ja': Pororo(task="ner", lang="ja"),
+}
+print("End to load Pororo")
 
 
 def identify_lang(text):
@@ -50,19 +61,54 @@ def isKoreanIncluded(text, check_to_num=-1):
     return False
 
 
+def replace_ner_to_special_token(text, lang):
+    ner_model = ner_models[lang]
+    ner_results = ner_model(text)
+    # print(ner_results)
+
+    ## ner 단어를 활용해서 대치하려고 했으나 어떤 경우에는 그 기호 안에 있는 ner도 해석해버리는 경우가 많고, 다른 단어 해석에도 영향을 미칠 수 있음
+    # ner_set = set()
+    # for ner_tuple in ner_results:
+    #     if ner_tuple[1] != 'O':  # NER
+    #         ner_set.add(ner_tuple[0])
+    # joined_ner = '|'.join(ner_set)
+    # prep_text = re.sub(f'(\W|^)({joined_ner})(\W|$)', r'\1[/\2/]\3', text)
+
+    ## 'NER1'과 같이 바꿔주고 다시 대치
+    ners = []
+    for ner_tuple in ner_results:
+        if ner_tuple[1] != 'O':  # NER
+            ners.append(ner_tuple[0])
+
+    for idx, ner in enumerate(ners):
+        text = re.sub(f'(\W|^)({ner})(\W|$)', rf"\1'NER{idx}'\3", text)
+    # # ' '.join(tokens)
+    # print(ners)
+
+    return text, ners
+
+def restore_ner(translated_text, ners):
+    # re.sub(r"'(NER)(\d+)'", rf"{ners[\2]}", translated_text)  # 변수 안에 reg ㅍ현이 들어가야 하는데 어떻게 하느거지..
+    for idx, ner in enumerate(ners):
+        translated_text = re.sub(rf"'NER{idx}'", ner, translated_text)
+
+    return translated_text
+
+
 if __name__ == '__main__':
     client_id = "PwtsQmgHn5h50GCthwtj"
     client_secret = "zNWtEXWpt_"
     text_ko = '오늘은 이상한 날이다' 
-    text_en = 'Today is a strange day.' 
+    text = 'Today is a strange day.' 
     target_lang = 'ko'
 
-    source_lang = 'ko' if isKoreanIncluded(text_ko, 100) else 'en'  #identify_lang(text)
+    source_lang = 'ko' if isKoreanIncluded(text, 100) else 'en'  #identify_lang(text)
     if source_lang not in CAN_LANG:
         raise ValueError
     
-    # prep_source_text, ners = replace_ner_to_special_token(text, source_lang)
+    prep_source_text, ners = replace_ner_to_special_token(text, source_lang)
+    print(prep_source_text)
     # translated_text = translate(prep_source_text, source_lang, target_lang, client_id, client_secret)
-    # result_text = restore_ner(translated_text, ners)
+    result_text = restore_ner(prep_source_text, ners)
 
-    print(source_lang)
+    print(result_text)
